7e3e9a1d244951f86fc7018cd4b2d78cb9bc0702
anarenee44 <anarenee9696@gmail.com>
2022-05-16 00:23:46 -0500

    project section pouplated

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { Flex } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
 
 const Projects: React.FC = () => {
-  return <div>3rd Projects</div>;
+  return (
+    <Flex
+      minW="100%"
+      h="625"
+      backgroundColor="orange.200"
+      ml="-517"
+      mr="-517"
+      alignItems="center"
+      direction="column"
+    >
+      <Flex paddingTop="20px">
+        <Text as="u" fontSize="4xl">
+          Projects
+        </Text>
+      </Flex>
+      <Container
+        minW="100%"
+        ml="-517"
+        mr="-517"
+        h="350"
+        backgroundColor="orange.100"
+        opacity="50%"
+        marginTop="90px"
+      ></Container>
+    </Flex>
+  );
 };
 
 export default Projects;
