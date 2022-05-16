ddffc9c8122b05007b76fdc1a74dbf1d3979d691
anarenee44 <anarenee9696@gmail.com>
2022-05-15 22:42:30 -0500

    home section container created

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,26 @@
 import React from "react";
-import { Container } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
 
 const Home: React.FC = () => {
-  return <Container>1st Home</Container>;
+  return (
+    <Flex
+      minW="100%"
+      h="625"
+      backgroundColor="blue.100"
+      ml="-517"
+      mr="-517"
+      justifyContent="center"
+      direction="column"
+    >
+      <Flex justifyContent="flex-start" paddingLeft="450px">
+        <Text fontSize="9xl">Anastasia</Text>
+      </Flex>
+      <Flex justifyContent="flex-end" paddingRight="500px">
+        <Text fontSize="9xl">Renee</Text>
+      </Flex>
+    </Flex>
+  );
 };
 
 export default Home;
